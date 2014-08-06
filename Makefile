a8b00ee75662b7fd22fd617194d8698066084159
Steve Rude <srude@riotgames.com>
2014-08-05 18:05:16 -0700

    Add makefile and Godeps

---
+++ Makefile
@@ -0,0 +1,26 @@
+bindir := bin
+outbin := ${bindir}/envd
+
+build: get_deps ${outbin}
+build_all: get_deps
+	go build -a -o ${outbin}
+${outbin}: dockver.go
+	go build -o ${outbin}
+clean:
+	rm -f ${bindir}/*
+	rm .deps
+get_deps: .deps
+.deps: Godeps/Godeps.json
+	godep restore
+	touch .deps
+test:
+	go test -v .
+fmt:
+	@status=$$(git status --porcelain | grep -v '??' | grep '.go$$' 2> /dev/null); \
+	if test "x$${status}" = x; then \
+		gofmt -w .; \
+	else \
+		echo "[ERROR] gofmt changes should be committed in their own commit.\n\tDo not run fmt without committing your changes first to avoid muddying your changes with formatting corrections.\n\tEither 'git reset --hard' or 'git commit' to run 'make fmt' or run 'gofmt' manually."; \
+		echo '\n\tChanged files:'; \
+		git status --porcelain | grep -v '??' | grep '.go$$' | awk '{printf "\t\t%s\n", $$2}' 2> /dev/null; \
+	fi
--- environment.go
+++ environment.go
@@ -1,67 +1,68 @@
 package main
 
 import (
-    "fmt"
-    "github.com/coreos/go-etcd/etcd"
-    "log"
-    "os"
-    "strings"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/coreos/go-etcd/etcd"
 )
 
 type Environment struct {
-    name      string
-    variables []*EnvironmentVariable
+	name      string
+	variables []*EnvironmentVariable
 }
 
 type EnvironmentVariable struct {
-    name  string
-    value string
+	name  string
+	value string
 }
 
 func NewEnvironment(name string, variables ...EnvironmentVariable) *Environment {
-    e := Environment{name, []*EnvironmentVariable{}}
-    env := os.Environ()
+	e := Environment{name, []*EnvironmentVariable{}}
+	env := os.Environ()
 
-    for i := 0; i < len(env); i++ {
-        envv := strings.Split(env[i], "=")
-        e.AddEnvironmentVariable(NewEnvironmentVariable(envv[0], envv[1]))
-    }
+	for i := 0; i < len(env); i++ {
+		envv := strings.Split(env[i], "=")
+		e.AddEnvironmentVariable(NewEnvironmentVariable(envv[0], envv[1]))
+	}
 
-    return &e
+	return &e
 }
 
 func (e *Environment) GetEnvironment() error {
-    results, err := etcd.Get(e.name)
-    if err != nil {
-        if len(results) > 0 {
-            log.Printf("Get failed with %s %s %v", results[0].Key, results[0].Value, results[0].TTL)
-        }
+	results, err := etcd.Get(e.name)
+	if err != nil {
+		if len(results) > 0 {
+			log.Printf("Get failed with %s %s %v", results[0].Key, results[0].Value, results[0].TTL)
+		}
 
-        return err
-    }
+		return err
+	}
 
-    for i := 0; i < len(results); i++ {
-        e.AddEnvironmentVariable(NewEnvironmentVariable(strings.Replace(results[i].Key, e.KeyName(), "", 1), results[i].Value))
-    }
-    return nil
+	for i := 0; i < len(results); i++ {
+		e.AddEnvironmentVariable(NewEnvironmentVariable(strings.Replace(results[i].Key, e.KeyName(), "", 1), results[i].Value))
+	}
+	return nil
 }
 
 func (e *Environment) KeyName() string {
-    return fmt.Sprintf("/%s/", e.name)
+	return fmt.Sprintf("/%s/", e.name)
 }
 
 func (e *Environment) AddEnvironmentVariable(ev *EnvironmentVariable) {
-    e.variables = append(e.variables, ev)
+	e.variables = append(e.variables, ev)
 }
 
 func (e Environment) Env() []string {
-    var stringArray = []string{}
+	var stringArray = []string{}
 
-    for i := 0; i < len(e.variables); i++ {
-        stringArray = append(stringArray, e.variables[i].String())
-    }
+	for i := 0; i < len(e.variables); i++ {
+		stringArray = append(stringArray, e.variables[i].String())
+	}
 
-    return stringArray
+	return stringArray
 }
 
 func (e Environment) Update() {
@@ -69,10 +70,10 @@ func (e Environment) Update() {
 }
 
 func NewEnvironmentVariable(name string, value string) *EnvironmentVariable {
-    ev := EnvironmentVariable{name, value}
-    return &ev
+	ev := EnvironmentVariable{name, value}
+	return &ev
 }
 
 func (ev *EnvironmentVariable) String() string {
-    return ev.name + "=" + ev.value
+	return ev.name + "=" + ev.value
 }
