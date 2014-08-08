cb479ab2665d785abf084f46a8c39ac04756ac69
Steve Rude <srude@riotgames.com>
2014-08-08 16:01:15 -0700

    fix build

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ echo "Killing off etcd if it is running..."
 killall etcd
 
 echo "Starting etcd..."
-nohup etcd &
+nohup etcd > /dev/null &
 sleep 3
 
 echo "Adding test variables..."
-etcdctl set VARIABLE1 "envd_var1"
-etcdctl set VARIABLE2 "envd_var2"
+etcdctl set /environments/test/VARIABLE1 "envd_var1"
+etcdctl set /environments/test/VARIABLE2 "envd_var2"
 
 echo "Running tests..."
 godep go test -v
